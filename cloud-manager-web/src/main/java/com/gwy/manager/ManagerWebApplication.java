c267193986700b4d2f803b5cd784b540327bf125
Tracyyy-s <supertracyyy@126.com>
2021-01-16 11:07:27 +0800

    add

---
+++ cloud-manager-web/src/main/java/com/gwy/manager/ManagerWebApplication.java
@@ -0,0 +1,16 @@
+package com.gwy.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Tracy
+ * @date 2021/1/16 11:05
+ */
+@SpringBootApplication
+public class ManagerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerWebApplication.class, args);
+    }
+}
