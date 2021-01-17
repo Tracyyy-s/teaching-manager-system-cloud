dccefdb83d7174398d59b275d87bc46d75a88b14
Tracyyy-s <supertracyyy@126.com>
2021-01-17 14:52:22 +0800

    update pom

--- cloud-manager-auth/src/main/java/com/gwy/manager/invokes/LoginInvoker.java
+++ cloud-manager-auth/src/main/java/com/gwy/manager/invokes/LoginInvoker.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.openfeign.FeignClientProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Tracy
